1f67e7d812bc6e578066b848097d138fe2cab069
xuyy121840 <xuyy163.com>
2019-07-13 15:11:15 +0800

    merge文件

--- examination/src/models/lookquestion.js
+++ examination/src/models/lookquestion.js
@@ -1,71 +1,35 @@
 
-import {lookquestion} from '../services/index.js'
-import {lookquestionExam} from '../services/index.js'
-import {lookquestionMenu} from '../services/index.js'
-import {lookquestionDetail} from '../services/index.js'
+import { lookquestion } from '../services/index.js'
+import { lookquestionExam } from '../services/index.js'
+import { lookquestionMenu } from '../services/index.js'
+import { lookquestionDetail } from '../services/index.js'
 export default {
-    // 命名空间
-    namespace: 'look',
-   // 模块的管理
-    state: {
-      isLookquestion:[],
-      isLookquestionExam:[],
-      isLookquestionMenu:[],
-      isLookquestionDetail:[]
-    },
-  
- 
-  
-  // 异步操作
-  //课程类型
-    effects: {  // generator
-      *lookquestion({payload},{call,put}){
-          let data = yield call(lookquestion);
-          // console.log('data...',data) 
-          // 调用同步的reduce的改变登录状态
-          yield put({
-              type:"updateLookquestion",// type是reducers里方法
-              payload: data.data // 相当于里面的执行
-          })
-      },
-
-     //考试类型
-      *lookquestionExam({payload},{call,put}){
-        let data = yield call(lookquestionExam);
-        console.log('data...',data) 
-        // 调用同步的reduce的改变登录状态
-        yield put({
-            type:"updateLookquestionExam",// type是reducers里方法
-            payload: data.data // 相当于里面的执行
-        })
-    },
-
-    //题目类型
-    *lookquestionMenu({payload},{call,put}){
-      let data = yield call(lookquestionMenu);
-      console.log('data...',data) 
-      // 调用同步的reduce的改变登录状态
-      yield put({
-          type:"updateLookquestionMenu",// type是reducers里方法
-          payload: data.data // 相当于里面的执行
-      })
+  // 命名空间
+  namespace: 'look',
+  // 模块的管理
+  state: {
+    isLookquestion: [],
+    isLookquestionExam: [],
+    isLookquestionMenu: [],
+    isLookquestionDetail: []
   },
 
-  *lookquestionDetail({},{call,put}){
-    let data = yield call(lookquestionDetail);
-    console.log('data...',data) 
-    // 调用同步的reduce的改变登录状态
-    yield put({
-        type:"updateLookquestionDetail",// type是reducers里方法
+
+
+  // 异步操作
+  //课程类型
+  effects: {  // generator
+    *lookquestion({ payload }, { call, put }) {
+      let data = yield call(lookquestion);
+      // console.log('data...',data) 
+      // 调用同步的reduce的改变登录状态
+      yield put({
+        type: "updateLookquestion",// type是reducers里方法
         payload: data.data // 相当于里面的执行
-    })
-},
-    
-
+      })
     },
-
     //考试类型
-    *lookquestionExam({ }, { call, put }) {
+    *lookquestionExam({ payload }, { call, put }) {
       let data = yield call(lookquestionExam);
       console.log('data...', data)
       // 调用同步的reduce的改变登录状态
@@ -74,6 +38,39 @@ export default {
         payload: data.data // 相当于里面的执行
       })
     },
+
+    //题目类型
+    *lookquestionMenu({ payload }, { call, put }) {
+      let data = yield call(lookquestionMenu);
+      console.log('data...', data)
+      // 调用同步的reduce的改变登录状态
+      yield put({
+        type: "updateLookquestionMenu",// type是reducers里方法
+        payload: data.data // 相当于里面的执行
+      })
+    },
+
+    *lookquestionDetail({ payload }, { call, put }) {
+      let data = yield call(lookquestionDetail);
+      console.log('data...', data)
+      // 调用同步的reduce的改变登录状态
+      yield put({
+        type: "updateLookquestionDetail",// type是reducers里方法
+        payload: data.data // 相当于里面的执行
+      })
+    },
+  },
+
+  //考试类型
+  *lookquestionExam({ payload }, { call, put }) {
+    let data = yield call(lookquestionExam);
+    console.log('data...', data)
+    // 调用同步的reduce的改变登录状态
+    yield put({
+      type: "updateLookquestionExam",// type是reducers里方法
+      payload: data.data // 相当于里面的执行
+    })
+  },
 
   // 同步操作
   reducers: { // 相当于派发
@@ -84,16 +81,16 @@ export default {
 
     updateLookquestionExam(state, action) {
       return { ...state, isLookquestionExam: action.payload };
-      },
-
-      updateLookquestionMenu(state, action) {
-        return { ...state,isLookquestionMenu:action.payload };
-      },
-
-
-      updateLookquestionDetail(state, action) {
-        return { ...state,isLookquestionDetail:action.payload };
-      },
-
     },
+
+    updateLookquestionMenu(state, action) {
+      return { ...state, isLookquestionMenu: action.payload };
+    },
+
+
+    updateLookquestionDetail(state, action) {
+      return { ...state, isLookquestionDetail: action.payload };
+    },
+
+  },
 };
